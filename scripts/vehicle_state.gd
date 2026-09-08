0bfc3d4b20c41880950873b87268d60d846f77bf
estecode <esteban.lind@gmail.com>
2026-09-09 00:06:32 +0200

    Remove vehicle state self-type coupling

--- scripts/vehicle_state.gd
+++ scripts/vehicle_state.gd
@@ -12,13 +12,13 @@ var z_m: float = 0.0
 var heading_rad: float = 0.0
 var speed_mps: float = 0.0
 
-func copy_from(other: VehicleState) -> void:
+func copy_from(other) -> void:
 	x_m = other.x_m
 	z_m = other.z_m
 	heading_rad = other.heading_rad
 	speed_mps = other.speed_mps
 
-func duplicate_state() -> VehicleState:
-	var result := VehicleState.new()
+func duplicate_state():
+	var result = get_script().new()
 	result.copy_from(self)
 	return result
