941be3a429974c70ee59c62017dca655e59a9a0e
estecode <esteban.lind@gmail.com>
2026-09-15 15:40:37 +0200

    Make GeoDot transition readiness driven

--- scripts/geodot_poc_tuned_main.gd
+++ scripts/geodot_poc_tuned_main.gd
@@ -1,7 +1,10 @@
 extends "res://scripts/geodot_poc_main.gd"
 
-const DETAIL_DISABLE_M := 60000.0
-const DETAIL_REENABLE_M := 50000.0
+const TransitionPolicy = preload("res://scripts/geodot_transition_policy.gd")
+const FAR_PRESENTATION_M := 55000.0
+
+var _transition_prefetching := false
+var _transition_detail_ready := false
 
 func _apply_tuning() -> void:
 	super._apply_tuning()
@@ -12,10 +15,32 @@ func _apply_tuning() -> void:
 
 func _update_distance_policy() -> void:
 	if not _geodot_ready: return
-	var distance := _camera_distance_to_focus(); var full_3d_distance:=maxf(250.0,float(_tuning.get("full_3d_distance_m",3000.0)))
-	var full_threshold:=clampf(float(_tuning.get("full_3d_threshold_px",4.0)),0.2,32.0)
+	var distance := _camera_distance_to_focus()
+	var full_3d_distance := maxf(250.0,float(_tuning.get("full_3d_distance_m",3000.0)))
+	var full_threshold := clampf(float(_tuning.get("full_3d_threshold_px",4.0)),0.2,32.0)
 	geodot_world_layer.set("far_exit_pixels",-1.0 if distance<=full_3d_distance else full_threshold)
-	if _detail_streaming_enabled and distance>=DETAIL_DISABLE_M:
-		_detail_streaming_enabled=false; geodot_world_layer.call("set_enabled",false)
-	elif not _detail_streaming_enabled and distance<=DETAIL_REENABLE_M:
-		_detail_streaming_enabled=true; geodot_world_layer.call("set_enabled",true)
+	var prefetch_scale := clampf(float(_tuning.get("prefetch_scale",1.35)),1.0,3.0)
+	var prefetch_distance := TransitionPolicy.prefetch_distance(FAR_PRESENTATION_M,prefetch_scale)
+	var want_detail := distance <= prefetch_distance
+	if want_detail != _detail_streaming_enabled:
+		_detail_streaming_enabled = want_detail
+		geodot_world_layer.call("set_enabled",want_detail)
+		_transition_prefetching = want_detail
+		_transition_detail_ready = false
+	if want_detail:
+		_transition_detail_ready = TransitionPolicy.detail_ready(geodot_world_layer.call("debug_snapshot"))
+	else:
+		_transition_detail_ready = false
+	if geodot_far_layer != null and geodot_far_layer.has_method("set_transition_hold"):
+		var hold_far := TransitionPolicy.hold_far(distance,FAR_PRESENTATION_M,want_detail,_transition_detail_ready)
+		geodot_far_layer.call("set_transition_hold",hold_far)
+
+func geodot_debug_snapshot() -> Dictionary:
+	var snapshot := super.geodot_debug_snapshot()
+	snapshot["transition"] = {
+		"prefetching": _transition_prefetching,
+		"detail_ready": _transition_detail_ready,
+		"prefetch_scale": float(_tuning.get("prefetch_scale",1.35)),
+		"prefetch_distance_m": TransitionPolicy.prefetch_distance(FAR_PRESENTATION_M,float(_tuning.get("prefetch_scale",1.35)))
+	}
+	return snapshot
